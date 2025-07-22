dac98b4cc092c562a99d197005e24724a3f0a704
Cauê Herison <caueherison290@gmail.com>
2025-07-22 16:05:21 -0300

    atualizações

--- src/components/ProdutosPage/ProdutosPage.jsx
+++ src/components/ProdutosPage/ProdutosPage.jsx
@@ -1,16 +1,23 @@
 import style from './ProdutosPage.module.scss'
 import { useState } from 'react';
+import shortJens from '../../assets/produtos-img/short-jens.png' 
+import shortAlf from '../../assets/produtos-img/short-alf.png' 
+import vestidoFlorido from '../../assets/produtos-img/vestido-florido.png' 
+import vestidoPoliamida from '../../assets/produtos-img/vestido-poliamida.png' 
+import conjuntoAzul from '../../assets/produtos-img/conjunto-azul.png' 
+import conjuntoPraia from '../../assets/produtos-img/conjunto-praia.png' 
 
 
 const produtos = [
-  { id: 1, nome: 'Jaqueta Rosa', preco: '159,90', categoria: 'Blusas', imagem: '' },
-  { id: 2, nome: 'Beigee', preco: '79,90', categoria: 'Blusas', imagem: '' },
-  { id: 3, nome: 'Jeans', preco: '199,90', categoria: 'Calças', imagem: '/images/jeans1.png' },
-  { id: 4, nome: 'Vestidess', preco: '199,90', categoria: 'Vestidos', imagem: '/images/vestido.png' },
-  { id: 5, nome: 'Guantré', preco: '39,90', categoria: 'Acessórios', imagem: '/images/bolsa.png' },
+  { id: 1, nome: 'Short jeans', preco: '159,90', categoria: 'Shorts', imagem: shortJens },
+  { id: 2, nome: 'Beigee', preco: '79,90', categoria: 'Shorts', imagem: shortAlf },
+  { id: 3, nome: 'Jeans', preco: '199,90', categoria: 'Vestidos', imagem: vestidoFlorido },
+  { id: 4, nome: 'Vestidess', preco: '199,90', categoria: 'Vestidos', imagem: vestidoPoliamida },
+  { id: 5, nome: 'Guantré', preco: '39,90', categoria: 'Acessórios', imagem: conjuntoAzul},
+  { id: 5, nome: 'Guantré', preco: '39,90', categoria: 'Acessórios', imagem: conjuntoPraia}
 ];
 
-const categorias = ['Todas', 'Blusas', 'Calças', 'Vestidos', 'Acessórios'];
+const categorias = ['Todas', 'Blusas', 'Calças', 'Vestidos', 'Shorts'];
 
 export default function ProdutosPage() {
   const [filtro, setFiltro] = useState('Todas');
